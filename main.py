4925394afc7fc61fbea7b02d9ecf2d84610d43e9
Tasnim Ferdous <tferdous611@gmail.com>
2023-10-07 17:13:03 -0400

    implement mean squared error function

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+
+data = pd.read_csv('MOCK_DATA.csv')
+
+def mean_squared_error(m, b, points):
+    total_error = 0
+    for i in range(len(points)):
+        x = points.iloc[i].x
+        y = points.iloc[i].y
+        total_error += (y - (m * x + b)) ** 2
+    total_error / float(len(points))
