32f82a2b5d8e66a416378852f845ea98ec7bc45d
Tasnim Ferdous <tferdous611@gmail.com>
2023-10-07 17:19:32 -0400

    implement gradient descent function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv('MOCK_DATA.csv')
 
+
 def mean_squared_error(m, b, points):
     total_error = 0
     for i in range(len(points)):
@@ -10,3 +11,24 @@ def mean_squared_error(m, b, points):
         y = points.iloc[i].y
         total_error += (y - (m * x + b)) ** 2
     total_error / float(len(points))
+
+
+def gradient_descent(m_now, b_now, points, L):
+    m_gradient = 0
+    b_gradient = 0
+
+    n = len(points)
+
+    for i in range(n):
+        x = points.iloc[i].x
+        y = points.iloc[i]. y
+
+        m_gradient += -(2/n) * x * (y - (m_now * x + b_now))
+        b_gradient += -(2/n) * (y - (m_now * x + b_now))
+
+    m = m_now - m_gradient * L
+    b = b_now * b_gradient * L
+
+    return m, b
+
+
